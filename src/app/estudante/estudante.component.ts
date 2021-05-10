a011d32e35ac348b5a35fbe8ac2a554adab2dffe
Diego Gonzales <diegogonzales@outlook.com.br>
2021-05-10 00:15:15 -0300

    Add mock-estudantes, update Estudante Css, Html and TypeScript

--- src/app/estudante/estudante.component.ts
+++ src/app/estudante/estudante.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Estudante } from '../estudante'
+import { ESTUDANTE } from '../mock-estudantes';
 
 @Component({
   selector: 'app-estudante',
@@ -8,12 +9,19 @@ import { Estudante } from '../estudante'
 })
 export class EstudanteComponent implements OnInit {
 
+  estudantes = ESTUDANTE;
+  selectedEstudante?: Estudante;
+
   estudante: Estudante = {
     ra: Math.floor((Math.random() * 100) + 1),
     name: "FULANO FULANES",
     telephone: 1530306254,
     class: "3B",
     motherName: "CICLANA FULANES"
+  }
+
+  onSelect(estudante: Estudante): void {
+    this.selectedEstudante = estudante;
   }
 
   constructor() { }
---
+++ src/app/mock-estudantes.ts
@@ -0,0 +1,15 @@
+import { Estudante } from './estudante';
+
+export const ESTUDANTE: Estudante[] = [
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Fulano Cici',telephone: 2024244, class: '3A', motherName: "Ciclana Cici", },
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Ciclana F.',telephone: 203532453, class: '1A', motherName: "Fulana F.", },
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Funalinha Ciclana',telephone: 23453450, class: '1B', motherName: "Otra Ciclana", },
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Maria Fulana',telephone: 453453820, class: '1C', motherName: "Fulana Maria", },
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Fernada Ciclana',telephone: 453455220, class: '3B', motherName: "Ciclana Fernada", },
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Ciclana Fulanes',telephone: 159873520, class: '3C', motherName: "Fulanes Ciclana", },
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Cici Cicla',telephone: 1589877820, class: '2A', motherName: "Fifi Cicla", },
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Maria Fula',telephone: 159875320, class: '2B', motherName: "Funalinha Fula", },
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Fifi Ciclana',telephone: 322574520, class: '2C', motherName: "Maria Ciclana", },
+     { ra: Math.floor((Math.random() * 100) + 1), name: 'Edu Fulano',telephone: 69787420, class: '4A', motherName: "Eduarda Fulana", },
+ 
+];
