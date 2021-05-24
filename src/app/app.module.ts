2e89322c5da85577e3214a3b7980f2ac549c5b84
Diego Gonzales <diegogonzales@outlook.com.br>
2021-05-23 21:04:45 -0300

    Part 3, add estudante-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EstudanteComponent } from './estudante/estudante.component';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { EstudanteDetailComponent } from './estudante-detail/estudante-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EstudanteComponent
+    EstudanteComponent,
+    EstudanteDetailComponent
   ],
   imports: [
     BrowserModule,
